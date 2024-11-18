1a15d26d25b0befc27a4a7d355705bf9dcd420b4
Karem Darwesh <karemdarwesh1035@gmail.com>
2024-11-18 21:39:27 +0200

    Controlled Elements

--- travel-list/src/App.js
+++ travel-list/src/App.js
@@ -1,7 +1,14 @@
+import { useState } from 'react';
+
+const initialItems = [
+  { id: 1, description: 'Passports', quantity: 2, packed: false },
+  { id: 2, description: 'Socks', quantity: 12, packed: false },
+  { id: 3, description: 'Charger', quantity: 1, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
-      <h3>h</h3>
       <Logo />
       <Form />
       <PackingList />
@@ -14,18 +21,61 @@ function Logo() {
   return <h1>🌴 Far Away 🏖️</h1>;
 }
 function Form() {
+  const [description, setDescription] = useState('');
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your 😍 trip</h3>
-    </div>
+      <select>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option key={num} value={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => {
+          e.preventDefault();
+          setDescription(e.target.value);
+        }}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 function PackingList() {
-  return <div className="list">LIST</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item key={item.id} item={item} />
+        ))}
+      </ul>
+    </div>
+  );
 }
+
+function Item({ item }) {
+  return (
+    <li>
+      <input type="checkbox" />
+      <span style={item.packed ? { textDecoration: 'line-through' } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button className="">❌</button>
+    </li>
+  );
+}
+
 function Stats() {
   return (
-    <footer>
+    <footer className="stats">
       <em>
         👜 You have x items on your list, and you already packes X (X%) of them.
       </em>
