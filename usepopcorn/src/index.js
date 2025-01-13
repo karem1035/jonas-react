cab2067d761f57f2dc3b48fab4f2dc04c3dc1d65
Karem Darwesh <karemdarwesh1035@gmail.com>
2025-01-13 06:17:47 +0200

    Small updates to popcorn project

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { useState } from 'react';
+import StarRating from './StarRating';
+
+function Test() {
+    const [rating, setRating] = useState(0);
+    return (
+        <div>
+            <StarRating onSetRating={setRating} maxRating={10} color="blue" />
+            <h1>This movie is {rating} stars</h1>
+        </div>
+    );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <StarRating
+            maxRating={5}
+            messages={['Terrible', 'Bad', 'Ok', 'Good', 'Great']}
+            defaultRating={3}
+            onSetRating={(rating) => console.log(rating)}
+        />
+        <Test />
     </React.StrictMode>
 );
